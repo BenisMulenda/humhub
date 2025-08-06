7593a42b206aa118594df844de198f18ae9bdf47
BenisMulenda <31537439+BenisMulenda@users.noreply.github.com>
2025-08-07 01:02:16 +0200

    Update common.php

--- protected/config/common.php
+++ protected/config/common.php
@@ -1,8 +1,33 @@
 <?php
-/**
- * This file provides to overwrite the default HumHub / Yii configuration by your local common (Console and Web) environments
- * @see http://www.yiiframework.com/doc-2.0/guide-concept-configurations.html
- * @see https://docs.humhub.org/docs/admin/advanced-configuration
- */
+
 return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=' . getenv('DB_HOST') . ';port=' . getenv('DB_PORT') . ';dbname=' . getenv('DB_NAME'),
+            'username' => getenv('DB_USER'),
+            'password' => getenv('DB_PASSWORD'),
+            'charset' => 'utf8mb4',
+            'enableSchemaCache' => true,
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'formatter' => [
+            'class' => 'yii\i18n\Formatter',
+            'defaultTimeZone' => 'UTC',
+            'timeZone' => 'UTC',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+    'params' => [
+        'installed' => true,
+    ],
 ];
