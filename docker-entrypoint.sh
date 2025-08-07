984cb4b73f445bff28d33b983787b7b3b62fcd3d
BenisMulenda <31537439+BenisMulenda@users.noreply.github.com>
2025-08-07 15:31:26 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Replace Apache config ports with Railway-provided $PORT
-if [[ -n "$PORT" ]]; then
-  echo "Using Railway PORT: $PORT"
+# Bind dynamic Railway port only if PORT is a valid number
+if [[ "$PORT" =~ ^[0-9]+$ ]]; then
+  echo "Using PORT: $PORT"
   sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
-  sed -i "s/:80>/:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+  sed -i "s/:80/:${PORT}/" /etc/apache2/sites-enabled/000-default.conf
+else
+  echo "Warning: PORT variable is invalid or not set. Using default port 80."
 fi
 
-# Start Apache in foreground
 exec "$@"
