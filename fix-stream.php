2d4fc048e03318a7537f5291375eda28d9f79761
BenisMulenda <31537439+BenisMulenda@users.noreply.github.com>
2025-08-07 20:12:28 +0200

    Update fix-stream.php

--- fix-stream.php
+++ fix-stream.php
@@ -1,13 +1,20 @@
 <?php
 
+// Change path to autoload.php if your structure differs
+require_once(__DIR__ . '/protected/vendor/autoload.php');
+require_once(__DIR__ . '/protected/humhub/components/Application.php');
+
+$config = require(__DIR__ . '/protected/config/web.php');
+
+// Boot HumHub in CLI-like mode
+\Yii::$app = new humhub\components\Application($config);
+
 use humhub\modules\activity\models\Activity;
 
-require_once(__DIR__ . '/protected/humhub/config/cli.php');
-
-$brokenActivities = Activity::find()->all();
 $count = 0;
+$activities = Activity::find()->all();
 
-foreach ($brokenActivities as $activity) {
+foreach ($activities as $activity) {
     try {
         $content = $activity->getContent();
         if (!$content || !$content->one()) {
